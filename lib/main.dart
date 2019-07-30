f470868d10e1b47997e452f811871ee22f526952
eshan <bajal.esh@gmail.com>
2019-07-30 21:27:32 +0530

    Third Edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:incubator_app/login_screen.dart';
 import 'package:incubator_app/registration_screen.dart';
+import 'package:incubator_app/otp_page.dart';
 
 void main() => runApp(FlashChat());
 
@@ -13,12 +14,13 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: 'login_screen',
+      initialRoute: 'otp_screen',
       routes: {
         //'welcome_screen': (context)=> WelcomeScreen(),
         //'chat_screen': (context)=> ChatScreen(),
         'login_screen':(context)=> LoginScreen(),
         'registration_screen': (context)=> RegistrationScreen(),
+        'otp_screen': (context) => Otp(),
       },
       // home: WelcomeScreen(),
     );
